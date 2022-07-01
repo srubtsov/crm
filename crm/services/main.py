a2e17e1fb68fab8d0362147da28249ba08a70708
Станислав Рубцов <stan@Stanislavs-MacBook-Air.local>
2022-07-02 00:01:28 +0300

    Add Cors Middleware to the FastApi app

--- crm/services/main.py
+++ crm/services/main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from .core import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
